369dd096cdfa5f5764e447f2b4b9a300623c51ff
Ismael López <ismaellopez.dev@gmail.comm>
2022-01-08 13:50:18 -0600

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'todo-list-sveltekit',
+  webDir: 'build',
+  bundledWebRuntime: false
+};
+
+export default config;
